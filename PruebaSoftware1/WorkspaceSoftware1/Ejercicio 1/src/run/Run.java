d3cd16e117b387de027c637e9bca42dd2aed922c
nelsNio <nels96@hotmail.es>
2017-08-09 14:59:53 -0500

    Adicion de la clase resta y test

--- PruebaSoftware1/WorkspaceSoftware1/Ejercicio 1/src/run/Run.java
+++ PruebaSoftware1/WorkspaceSoftware1/Ejercicio 1/src/run/Run.java
@@ -5,7 +5,11 @@
  */
 package run;
 
+import logic.Substract;
+
 public class Run {
 	public static void main(String[] args) {
+		Substract subject = new Substract();
+		System.out.println(subject.subtract(8, 5));
 	}
 }

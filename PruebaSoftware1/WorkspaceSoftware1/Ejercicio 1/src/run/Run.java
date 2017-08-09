fd283c751f8c553406fd22284d6b98d27e442f55
nelsNio <nels96@hotmail.es>
2017-08-09 14:38:07 -0500

    Prueba clon para suma

---
+++ PruebaSoftware1/WorkspaceSoftware1/Ejercicio 1/src/run/Run.java
@@ -0,0 +1,11 @@
+/**
+ * @autor Duvis Alejando Gómez Neira
+ * @autor Nelson Andrés Niño Verdugo
+ * @autor Ángel Gutiérrez Guerrero
+ */
+package run;
+
+public class Run {
+	public static void main(String[] args) {
+	}
+}
